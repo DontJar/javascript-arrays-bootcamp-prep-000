fa58ff3e8d8ba7375e465ddd425e37270e3e7fac
Jon Schulman <schulman99@gmail.com>
2018-05-13 20:21:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,6 @@ var ingredient6 = "garlic";
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray (array, element) {
-  return ['...array', ${element}]
+function addElementToBeginningOfArray(array, element) {
+  return '...array', element
 }
