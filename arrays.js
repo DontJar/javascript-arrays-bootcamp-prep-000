e11545295243c582c2a558f734b322da75066b03
Jon Schulman <schulman99@gmail.com>
2018-05-13 19:56:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,4 +11,4 @@ function addElementToBeginningOfArray (array, element) {
   return ...array, element
 }
 
-Function destructivelyAddElementToBeginningOfArray
+// Function destructivelyAddElementToBeginningOfArray
