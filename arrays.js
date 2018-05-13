cc570d82dee9360e967fbb8cedbce2150bccfc61
Jon Schulman <schulman99@gmail.com>
2018-05-13 19:56:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,14 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+var ingredient1 = "bread";
+var ingredient2 = "mild cheese";
+var ingredient3 = "sharp cheese";
+var ingredient4 = "butter";
+var ingredient5 = "tomato";
+var ingredient6 = "garlic";
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+function addElementToBeginningOfArray (array, element) {
+  return ...array, element
+}
+
+Function destructivelyAddElementToBeginningOfArray
