a8ca505f9404e82e1d13bb07910e85fc7c52be22
Jon Schulman <schulman99@gmail.com>
2018-05-14 00:28:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,9 +32,11 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
+  array.shift();
   
+  return array;
 }
+
 
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
