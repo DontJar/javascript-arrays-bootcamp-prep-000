206f5eac7f27b165ce52543b2b5620b87bc2aa4c
Jon Schulman <schulman99@gmail.com>
2018-05-14 00:34:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
   }
+
+function removeElementFromEndOfArray(array) {
+  return array.pop();
+}
